079b29167d045832776e7653e710619f31e12e1f
Roque Javier Ducuara Sosa <sadalmelik828@gmail.com>
2025-07-07 11:52:21 -0500

    Replace Camunda by Eximee BPMS engine libraries.

--- src/main/java/com/nttdata/camunda/application/services/camunda/CamundaInstance.java
+++ src/main/java/com/nttdata/camunda/application/services/camunda/CamundaInstance.java
@@ -1,12 +1,12 @@
 package com.nttdata.camunda.application.services.camunda;
 
-import org.camunda.bpm.application.impl.event.ProcessApplicationEventListenerPlugin;
-import org.camunda.bpm.engine.impl.cfg.ProcessEnginePlugin;
-import org.camunda.bpm.engine.impl.plugin.AdministratorAuthorizationPlugin;
-import org.camunda.bpm.engine.rest.security.auth.ProcessEngineAuthenticationFilter;
-import org.camunda.bpm.engine.rest.security.auth.impl.HttpBasicAuthenticationProvider;
-import org.camunda.connect.plugin.impl.ConnectProcessEnginePlugin;
-import org.camunda.spin.plugin.impl.SpinProcessEnginePlugin;
+import org.eximeebpms.bpm.application.impl.event.ProcessApplicationEventListenerPlugin;
+import org.eximeebpms.bpm.engine.impl.cfg.ProcessEnginePlugin;
+import org.eximeebpms.bpm.engine.impl.plugin.AdministratorAuthorizationPlugin;
+import org.eximeebpms.bpm.engine.rest.security.auth.ProcessEngineAuthenticationFilter;
+import org.eximeebpms.bpm.engine.rest.security.auth.impl.HttpBasicAuthenticationProvider;
+import org.eximeebpms.connect.plugin.impl.ConnectProcessEnginePlugin;
+import org.eximeebpms.spin.plugin.impl.SpinProcessEnginePlugin;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
